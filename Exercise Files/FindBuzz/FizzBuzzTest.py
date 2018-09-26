715ab90fffb980ca53c72d316e53e6aed1dfafca
R-en <renli@uw.edu>
2018-09-25 22:10:37 -0700

    Unit test on FizzBuzz problem completed

--- Exercise Files/FindBuzz/FizzBuzzTest.py
+++ Exercise Files/FindBuzz/FizzBuzzTest.py
@@ -1,4 +1,38 @@
 import pytest
 
-def test_canCallFizzBuzz():
-    fizzBuzz(1)
+def fizzBuzz(value):
+    if isMultiple(value,3):
+        if isMultiple(value, 5):
+            return "FizzBuzz"
+        return "Fizz"
+    if isMultiple(value, 5):
+        return "Buzz"
+    return str(value)
+
+def isMultiple(value, mod):
+    return (value % mod) == 0
+
+def checkFizzBuzz(value, expectedRetVal):
+    retVal = fizzBuzz(value)
+    assert retVal == expectedRetVal
+
+def test_return1With1PassedIn():
+    checkFizzBuzz(1, "1")
+
+def test_return2With2PassedIn():
+    checkFizzBuzz(2, "2")
+
+def test_returnFizzWith3PassedIn():
+    checkFizzBuzz(3, "Fizz")
+
+def test_returnBuzzWith5PassedIn():
+    checkFizzBuzz(5, "Buzz")
+
+def test_returnFizzWith6PassedIn():
+    checkFizzBuzz(6, "Fizz")
+
+def test_returnBuzzWith10PassedIn():
+    checkFizzBuzz(10, "Buzz")
+
+def test_returnFizzBuzzWith15PassedIn():
+    checkFizzBuzz(15, "FizzBuzz")
